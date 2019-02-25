9da14e133df74f9e5730fac5a340a3e2894f8619
Steve Gong <stevegong2016@gmail.com>
2019-02-26 10:57:35 +1100

    installed social_feed_fetcher module in patched directory

---
+++ modules/patched/social_feed_fetcher/src/Plugin/NodeProcessor/FacebookNodeProcessor.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\social_feed_fetcher\Plugin\NodeProcessor;
+
+use Drupal\node\Entity\Node;
+use Drupal\social_feed_fetcher\PluginNodeProcessorPluginBase;
+
+/**
+ * Class FacebookNodeProcessor
+ *
+ * @package Drupal\social_feed_fetcher\Plugin\NodeProcessor
+ *
+ * @PluginNodeProcessor(
+ *   id = "facebook_processor",
+ *   label = @Translation("Facebook node processor")
+ * )
+ */
+class FacebookNodeProcessor extends PluginNodeProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function processItem($source, $data_item) {
+    if (!$this->isPostIdExist($data_item['id'])) {
+      $node = $this->entityStorage->create([
+        'type' => 'social_post',
+        'title' => 'Post ID: ' . $data_item['id'],
+        'field_platform' => ucwords($source),
+        'field_id' => $data_item['id'],
+        'field_post' => [
+          'value' => social_feed_fetcher_linkify(html_entity_decode($data_item['message'] ?: '')),
+          'format' => $this->config->get('formats_post_format'),
+        ],
+        'field_social_feed_link' => [
+          'uri' => $data_item['link'],
+          'title' => '',
+          'options' => [],
+        ],
+        'field_sp_image' => [
+          'target_id' => $this->processImageFile($data_item['image'], 'public://facebook'),
+        ],
+        'field_posted' => [
+          'value' => $this->setPostTime($data_item['created_time']),
+        ],
+      ]);
+      return $node->save();
+    }
+    return FALSE;
+  }
+
+  /**
+   * Save external file.
+   *
+   * @param $filename
+   * @param $path
+   *
+   * @return int
+   */
+  public function processImageFile($filename, $path) {
+    $data = file_get_contents($filename);
+    $uri = $path . '/' . mt_rand() . '.jpg';
+    file_prepare_directory($path, FILE_CREATE_DIRECTORY);
+    return file_save_data($data, $uri, FILE_EXISTS_REPLACE)->id();
+  }
+
+}
