052949c126ba3f141a3669080a6f6eeb8d9dbc75
Jason Guo <jason@xing.com.au>
2019-03-20 22:27:57 +1100

    slide text to have parallex effect

--- themes/custom/expo2020/assets/src/js/app/scrollmagic.js
+++ themes/custom/expo2020/assets/src/js/app/scrollmagic.js
@@ -48,25 +48,45 @@ export default class Scrollmagic {
                 return elementBottom > viewportTop && elementTop < viewportBottom;
             };
 
+              $.fn.isScrollingOut = function() {
+                var elementTop = $(this).offset().top;
+                var elementBottom = elementTop + $(this).outerHeight();
+
+                var viewportTop = $(window).scrollTop();
+
+                return elementTop == viewportTop;
+              };
+
             $(window).on('resize scroll', function() {
                 $('.panel').each(function() {
                     var slideID = $(this).attr('id');
-                    var elems = $("#txt-" + slideID);
+                    if ($(this).isScrollingOut()) {
+                      var txtElemt = $("#txt-" + slideID);
+                      if (slideID == 'slide1') {
+                        var nextSlide = $('#slide2').offset();
+                      }
+                      if (slideID == 'slide0') {
+                        var nextSlide = $('#slide1').offset();
+                      }
+                      if(typeof nextSlide != 'undefined') {
+                          var txtOffsetY = 1.25 * (nextSlide.top - ($(this).outerHeight() + $(this).offset().top));
+                          $("#txt-" + slideID).css({ transform: 'translate(0,' + txtOffsetY + 'px)'});
+                      }
+                    }
                     if ($(this).isInViewport()) {
                         $('.pips li div').removeClass('active');
                         $('#pip-' + slideID).addClass('active');
                         /*$('.slide').removeClass('active remove');
                         $("#txt-" + slideID).addClass("active", 2000);*/
-                        elems.css({ transform: 'translate(0, -' + window.pageYOffset / 25 + 'px)',
-                            transition: 'transform 300ms ease'});
+
                         /*elems.animate({
                             transform: 'translate(0, -' + window.pageYOffset / 30 + 'px)'
                         }, 'fast');*/
                     } else {
                         $('#pip-' + slideID).removeClass('active');
                         /* $("#txt-" + slideID).addClass("remove", 2000);*/
-                        elems.css({ transform: 'translate(0, ' + window.pageYOffset / 25 + 'px)',
-                            transition: 'transform 300ms ease'});
+                        // elems.css({ transform: 'translate(0, ' + window.pageYOffset / 25 + 'px)',
+                        //     transition: 'transform 300ms ease'});
                     }
                 });
             });
